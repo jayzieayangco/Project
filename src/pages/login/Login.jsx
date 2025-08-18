41017da2fb41b427a193721ee75be8b4f1066003
jz <jayzie.absoluteisi@gmail.com>
2025-08-18 15:35:22 +0800

    refactor: added dashboard redirect and tailwind

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const navigate = useNavigate();
+
+  function navigateTo(path) {
+    navigate(path);
+  }
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <span>Welcome to the Login Page.</span>
+      <button
+        onClick={() => navigateTo("/dashboard")}
+        className="p-2 rounded-lg border-1 border-blue- bg-blue-500 text-white hover:bg-transparent hover:text-blue-500 transition-colors duration-300 cursor-pointer"
+      >
+        Proceed to dashbaord here.
+      </button>
     </div>
   );
 }
