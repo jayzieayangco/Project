d60b22919a221b4b6c69c633f187eda3d22bfc17
jz <jayzie.absoluteisi@gmail.com>
2025-08-18 17:30:17 +0800

    Test: Experimented on Input (Unfinished)

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,20 @@
+import React, { useState, useEffect} from "react";
+
 export default function Dashboard() {
+  const [Email, setEmail] = useState('');
+  const [Password, setPassword] = useState('');
+  const [ submitted, setSubmitted] = useState(false);
+  
   return (
-    <div>
-      <span>This is the dashboard.</span>
+    <div className="flex flex-col items-center justify-center h-screen bg-[#faf5ef]">
+      <span className="font-knewave text-8xl">DAILY TIME</span>      
+      <span className="font-knewave text-8xl">REPORT</span>
+      <button
+        onClick={() => navigateTo("/dashboard")}
+        className="font-overpass p-2 rounded-lg border-1 border-blue- bg-green-500 text-white hover:bg-transparent hover:text-blue-500 transition-colors duration-300 cursor-pointer"
+      >
+        Email: 
+      </button>
     </div>
   );
 }
