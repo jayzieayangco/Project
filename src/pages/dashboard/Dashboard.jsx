901befc59adcca479aec0194480aec605de8408f
jz <jayzie.absoluteisi@gmail.com>
2025-08-19 09:45:12 +0800

    Chore: Navigation Handling from Dashboard to Records

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,19 +1,23 @@
 import React, { useState, useEffect} from "react";
+import { useNavigate } from "react-router-dom"
 
 export default function Dashboard() {
-  const [Email, setEmail] = useState('');
-  const [Password, setPassword] = useState('');
-  const [ submitted, setSubmitted] = useState(false);
+  const navigate=useNavigate();
+
+  function navigateTo(path) {
+    navigate(path);
+  }
+  
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-[#faf5ef]">
       <span className="font-knewave text-8xl">DAILY TIME</span>      
       <span className="font-knewave text-8xl">REPORT</span>
       <button
-        onClick={() => navigateTo("/")}
+        onClick={() => navigateTo("/records")}
         className="font-overpass p-2 rounded-lg border-1 border-blue- bg-green-500 text-white hover:bg-transparent hover:text-blue-500 transition-colors duration-300 cursor-pointer"
       >
-        Email: 
+        ahm
       </button>
     </div>
   );
