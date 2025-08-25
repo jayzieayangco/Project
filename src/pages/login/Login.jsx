ddf8a6dfca0b20596c6d435047479e2c8aba0cab
jz <jayzie.absoluteisi@gmail.com>
2025-08-25 09:27:48 +0800

    Refactor: Cleaned Log In Page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,44 +1,68 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { supabase } from "../../supabase/supabase-client";
+import { supabaseLoginWithEmail } from "./Service";
 
 export default function Login() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  function navigateToDashboard(e) {
-    if (e) e.preventDefault();
-    navigate("/dashboard");
+  function navigateTo(route) {
+    navigate(route);
   }
 
-  useEffect(() => {
-    const handleKeyDown = (e) => {
-      if (e.key === "Enter") {
-        navigateToDashboard();
-      }
-    };
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const response = await supabaseLoginWithEmail(email, password);
 
-    window.addEventListener("keydown", handleKeyDown);
-    return () => window.removeEventListener("keydown", handleKeyDown);
-  }, []);
+    if (!response) {
+      return;
+    }
+
+    navigateTo("/records")
+    console.log("Logged in successfully.")
+  }
 
   return (
-    <div className="flex flex-col items-center h-screen bg-[#faf5ef]">
+    <div className="flex flex-col items-center justify-left h-max bg-[#faf5ef]">
       <div className="flex flex-col items-center justify-center p-30">
         <span className="font-knewave text-8xl">DAILY TIME</span>
         <br />
-        <span className="font-knewave text-8xl items-center">RECORD</span>
+        <span className="font-knewave text-8xl">RECORD</span>
       </div>
 
       <form
-        onSubmit={navigateToDashboard}
-        className="flex flex-col items-center justify-center"
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-2 items-center mb-2"
       >
-        <button
-          type="submit"
-          className="font-overpass p-2 rounded-lg border-1 border-blue- bg-green-600 text-white px-10 py-2 text-3xl font-bold m-9 hover:bg-transparent hover:text-black transition-colors duration-300 cursor-pointer"
-        >
-          LOG IN
+        <input
+          required
+          type="email"
+          placeholder="Email:"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="font-overpass px-2 py-2 text-xl rounded-lg border-1 border-blue bg-green-600 text-white font-bold hover:text-white-500 transition-colors duration-300"
+        />
+        <input
+          required
+          type="password"
+          placeholder="Password:"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="font-overpass p-2 text-xl rounded-lg border-1 border-blue- bg-green-600 text-white font-bold hover:text-white-500 transition-colors duration-300"
+        />
+
+        <button type="submit" className={`${button}`}>
+          Login
         </button>
       </form>
+      <button onClick={() => navigateTo("/register")} className={`${button}`}>
+        Register
+      </button>
     </div>
   );
 }
+
+export const button =
+  "font-overpass w-[200px] bg-green-600 text-white py-3 rounded-lg hover:bg-green-700 transition disabled:opacity-50";
