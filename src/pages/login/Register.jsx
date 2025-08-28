64eb7dfedbbe484543edb4222c56395bbe13572a
jz <jayzie.absoluteisi@gmail.com>
2025-08-28 09:50:08 +0800

    Chore: Added Name Input in Register

--- src/pages/login/Register.jsx
+++ src/pages/login/Register.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { supabase } from "../../supabase/supabase-client"; 
+import { supabase } from "../../supabase/supabase-client";
 
 export default function Register() {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -15,6 +16,7 @@ export default function Register() {
     setMessage("");
 
     const { data, error } = await supabase.auth.signUp({
+      name,
       email,
       password,
     });
@@ -30,6 +32,7 @@ export default function Register() {
     if (user) {
       const { error: insertError } = await supabase.from("users").insert([
         {
+          name,
           user_id: user.id,
           email: user.email,
         },
@@ -44,18 +47,17 @@ export default function Register() {
     }
 
     setMessage("Check your email to confirm your account.");
+    setName("");
     setEmail("");
     setPassword("");
     setLoading(false);
 
-    // ✅ After successful register, go back to login
     navigate("/");
   };
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-[#faf5ef] p-8 rounded-2xl shadow-xl w-96 relative">
-        
         <button
           className="absolute top-2 right-3 text-gray-500 hover:text-black text-2xl"
           onClick={() => navigate("/")}
@@ -68,6 +70,15 @@ export default function Register() {
         </h2>
 
         <form onSubmit={handleRegister} className="flex flex-col">
+          <input
+            type="name"
+            placeholder="Name"
+            className="font-overpass w-full p-3 mb-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+
           <input
             type="email"
             placeholder="Email"
